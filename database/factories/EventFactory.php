2484f57d478410987bc82a82b86b27070fcf1b45
Ramzi <ramzizerr@gmail.com>
2019-12-12 18:33:24 +0100

    Seeders

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,10 +3,24 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Event;
+use App\User;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Event::class, function (Faker $faker) {
     return [
-        //
+        'nom' => $faker->word,
+        'date' => Carbon::now(),
+        'late' => $faker->randomDigit,
+        'long' => $faker->randomDigit,
+        'theme' => $faker->word,
+        'effectif_max' => $faker->randomDigit,
+        'effectif_min' => $faker->randomDigit,
+        'descriptif' => $faker->text,
+        'user_id' => User::all()->random()->id,
+        'nom_rue' => $faker->text,
+        'ville' => $faker->word,
+        'codepostal' => $faker ->randomDigit,
+        'complement' => $faker ->word
     ];
 });
---
+++ database/seeds/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 50)->create()->each(function ($user) {
+            $user->event()->save(factory(App\Event::class)->make());
+        });
+    }
+}
